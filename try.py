c2d20edbef588b9ff084445be0659b7cb942dbb1
Jakob Jung <jakob.jung@fau.de>
2021-09-17 18:00:24 +0200

    RRemoved try.py

--- try.py
+++
@@ -1,8 +0,0 @@
-from tracklists import *
-
-url = "https://www.1001tracklists.com/tracklist/1fy61y0k/lane-8-fall-2021-mixtape-2021-09-15.html"
-if __name__ == '__main__':
-    print('This is a module')
-    tl = Tracklist(url)
-    tracks = tl.tracks
-    tracks = tracks
